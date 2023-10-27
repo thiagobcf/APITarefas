730261920514f17bff9d02d9a002821d7dab002d
thiagobcf <thiagobcfonseca@gmail.com>
2023-10-27 12:51:59 -0300

    Add Route Mapget, MapPost

--- APITarefas/Program.cs
+++ APITarefas/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("TarefasDB"));
 
 var app = builder.Build();
 
@@ -18,4 +23,34 @@ app.MapGet("frases", async () =>
    await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
 );
 
+app.MapGet("/tarefas", async (AppDbContext db) => 
+{
+    return await db.Tarefas.ToListAsync();
+});
+
+app.MapPost("/tarefas", async(Tarefa tarefa, AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
+
+
 app.Run();
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
+
